1e2947201acbeef53a98f5a7ebb147b5369b0b7f
Josue Benjamin <josuebcenturion@gmail.com>
2022-09-20 15:55:15 -0300

    updating images and media queries

--- lesson2/scripts/main.js
+++ lesson2/scripts/main.js
@@ -2,5 +2,5 @@ const y = new Date();
 let year = y.getFullYear();
 const date = new Object (document.lastModified);
 
-document.getElementById("year").innerHTML = `&copy; ${year} | Josué B. Centurión | Last Updated: ${date}; ` 
+document.getElementById("year").innerHTML = `&copy; ${year} | Josué B. Centurión | Last Updated: ${date}` 
 
