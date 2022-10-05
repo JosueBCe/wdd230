1eb4b903b7a9c606bc07a0597b0d20f0f80c4a61
Josue Benjamin <josuebcenturion@gmail.com>
2022-10-05 20:17:20 -0300

    centering content and adjusting distance in text

---
+++ chamber/js/scripts.js
@@ -0,0 +1,16 @@
+toggleMenu = () => {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+
+}
+const button = document.getElementById("hamburgerBtn")
+
+button.onclick = () => toggleMenu()
+
+
+const d = new Date()
+document.querySelector("#year").innerHTML = d.getFullYear();
+
+
+const date = new Object (document.lastModified);
+document.getElementById("updated").innerHTML =` Last Modification: ${date}`;
