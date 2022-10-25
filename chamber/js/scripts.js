f2f7e1dcfda3393ed33bdc44f3c61c4a32d45460
Josue Benjamin <josuebcenturion@gmail.com>
2022-10-24 23:56:43 -0300

    changing images and adding discover page

--- chamber/js/scripts.js
+++ chamber/js/scripts.js
@@ -35,3 +35,30 @@ display()
 function myFunction() {
     document.getElementById("myDIV").style.display = "none";
 }
+
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+
+
+const loadingImages = (img) => {
+    img.setAttribute("src", img.getAttribute("data-src"));
+    img.onload = () => {img.removeAttribute("data-src");}
+}
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadingImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    });
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadingImages(img);
+    });
+}
