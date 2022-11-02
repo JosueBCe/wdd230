31922d3a056b29f83a81a4a1dbd837595d7f2a51
Josue Benjamin <josuebcenturion@gmail.com>
2022-11-02 15:48:39 -0300

    header and footer

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ const y = new Date();
 let year = y.getFullYear();
 const date = new Object(document.lastModified);
 
-document.getElementById("year").innerHTML = `&copy; ${year} || Josué B. Centurión || Resistencia`;
+document.getElementById("year").innerHTML = `&copy; ${year}|| Josué B. Centurión || Resistencia `;
 
 document.getElementById("updated").innerHTML = `Last Updated: ${date}`;
 
