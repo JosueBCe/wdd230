697d1067c6a3e0ce0f769a7526bd79d7e6a520e2
Josue Benjamin <josuebcenturion@gmail.com>
2022-12-02 14:39:56 -0300

    updating order, local Storage

--- bountiful_foods/js/general.js
+++ bountiful_foods/js/general.js
@@ -1,58 +1,63 @@
 
-let titl = document.title
+// let titl = document.title
 
-
+// Gets the date html element 
 let dateSubmitted = document.querySelector("#date")
 
+// Hide the orders section at the beginning 
+document.querySelector(".orders").style.display = "none"
 
-
-
-
+let count = 0
 const url = "https://brotherblazzard.github.io/canvas-content/fruit.json";
-    const form  = document.getElementById('order');
+const form = document.getElementById('order');
+
+// Adapting Local Storage to be able to store list or arrays
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
 async function mainFresh() {
     try {
         //Fetch the Data from the API 
         const response = await fetch(url);
         if (response.ok) {
+
             // Make the data in json form
             const data = await response.json()
             console.log(data)
+
             // Adding options distrubuited between the 3 select options HTML elements
-            document.getElementById("select-1").innerHTML =  displayOptions(data.slice(0,13))
-            document.getElementById("select-2").innerHTML =  displayOptions(data.slice(13,27))
-            document.getElementById("select-3").innerHTML =  displayOptions(data.slice(27,39))
-   
-            
+            document.getElementById("select-1").innerHTML = displayOptions(data.slice(0, 13))
+            document.getElementById("select-2").innerHTML = displayOptions(data.slice(13, 27))
+            document.getElementById("select-3").innerHTML = displayOptions(data.slice(27, 39))
+
+
             form.addEventListener('submit', (event) => {
-            // handle the form data
+                // handle the form data and prevents of updating the page
                 event.preventDefault()
-                // userData = document.querySelectorAll("input,select")
+                // Gets the values of the Submitted Form 
                 let inputs = Array.from(form.elements)
-    
                 const values = inputs.map(e => e.value)
 
-                let askedFruits = values.slice(5,8)
+                console.log(values)
+                localStorage.set
+                // Gets the fruits asked 
+                let askedFruits = values.slice(5, 8)
+             
+                count += 1
+                localStorage.setObj(`Order #${count}`, askedFruits)
                 let nutritions = []
                 data.map(e => askedFruits.includes(e.name) ? nutritions.push(e.nutritions) : e)
-            
-                console.log(askedFruits)
-                console.log(nutritions)
-                
-                
-                
-                
-            
-                document.getElementById("submittedOrder").innerHTML += displayOrder(values)
-                document.getElementById("nutrional-info").innerHTML += `<li> Calories: ${sumNutritions(nutritions, "calories")}</li>`
-                document.getElementById("nutrional-info").innerHTML += `<li> Calories: ${sumNutritions(nutritions, "fat")}</li>`
-                document.getElementById("nutrional-info").innerHTML += `<li> Carbohydrates: ${sumNutritions(nutritions, "carbohydrates")}</li>`
-                document.getElementById("nutrional-info").innerHTML += `<li> Protein: ${sumNutritions(nutritions, "protein")}</li>`
-                document.getElementById("nutrional-info").innerHTML += `<li> Sugar: ${sumNutritions(nutritions, "sugar")}</li>`
-                
-                document.getElementById("submittedOrder").innerHTML += d.toLocaleDateString('en-us', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
-        });
 
+                // Show the orders section 
+                document.querySelector(".orders").style.display = "block"
+
+                // Shows One order with the user information and nutrients 
+                document.querySelector(".orders").innerHTML += displayOrder(values) + nutrientsSection(nutritions)
+            });
 
         } else {
             throw Error(await response.text());
@@ -65,20 +70,42 @@ async function mainFresh() {
 mainFresh()
 
 const displayOrder = (formValues) => {
-    necessaryValues = [2,3,5,6,7]
-    html = formValues.map((value,index) => necessaryValues.includes(index) ? `
-    <li>${value}</li>`:""
+
+    div = `<div class="order">
+    <h3>Order #${count}</h3>
+    <h4>Your Info.</h4>
+    `
+    necessaryValues = [1,2, 3, 5, 6, 7]
+    html = formValues.map((value, index) => necessaryValues.includes(index)
+        ? `<li>${value}</li>` :
+        index == 4 ? `<li class="subtitle"><strong>Ingridients of your mix
+        </strong></li> `
+            : ""
     ).join("")
 
-    return html
+    return div + html
 }
 
+const nutrientsSection = (nutritions) => {
+    nutrientSection = `
+    <div>
+    <h4>Nutritional Information</h4>
+    <ul>
+    <li> Calories: ${sumNutritions(nutritions, "calories")}</li>
+    <li> Fats: ${sumNutritions(nutritions, "fat").toFixed(2)}</li>
+    <li> Carbohydrates: ${sumNutritions(nutritions, "carbohydrates").toFixed(2)}</li>
+    <li> Protein: ${sumNutritions(nutritions, "protein").toFixed(2)}</li>
+    <li> Sugar: ${sumNutritions(nutritions, "sugar").toFixed(2)}</li>
+    </ul>
+    </div>
+    `
+    return nutrientSection
+}
 
 const sumNutritions = (nutrients, nutrient) => {
-    sum = nutrients.reduce(function(sum, current) {
-         return sum + current[`${nutrient}`];
-       }, 0) 
-
+    sum = nutrients.reduce(function (sum, current) {
+        return sum + current[`${nutrient}`];
+    }, 0)
     return sum
 }
 
@@ -89,7 +116,7 @@ const displayOptions = (data) => {
     ).join("")
     selectDefault = '<option value="please"> Select One Option </option>'
     return selectDefault + options
-} 
+}
 
 
 
